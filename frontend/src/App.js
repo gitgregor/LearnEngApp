342a643fe9ce3645bb0c7f029adfe31680078ef1
Gregory <gerda.trust@gmail.com>
2021-07-13 22:16:16 +0100

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Learn English App</h1>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+
+// <div className="App">
+//      </div> 
+
+// <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
+// </header>
